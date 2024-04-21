1734c97d87948118281a1f4e8dd6d674defb296d
SagarD1714 <142889676+SagarD1714@users.noreply.github.com>
2024-04-21 16:26:27 +0530

    adminside

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home,name="home"),
     path('temp', views.temp,name="temp"),
+    path('team', views.team_view, name='team_view'),
+    path('finance', views.finance_view, name='finance_view'),
+    path('work', views.work_view, name='work_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, redirect
 
 # Create your views here.
 def temp(request):
+    if request.method=="POST":
+        if 'team' in request.POST:
+            return redirect('team_view')
+        if 'finance' in request.POST:
+            return redirect('finance_view')
+        if 'work' in request.POST:
+            return redirect('work_view')
     return render(request, 'adminHome.html')
+
 
 def home(request):
     return render(request, 'login.html')
+
+def team_view(request):
+    # Your logic for team management view
+    return render(request, 'adminteam.html')
+
+
+def finance_view(request):
+    # Your logic for finance management view
+    return render(request, 'adminfinance.html')
+
+
+def work_view(request):
+    # Your logic for work staging view
+    return render(request, 'adminwork.html')
