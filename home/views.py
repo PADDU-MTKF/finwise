67306037a1d505cb278771e9196a99391dd1341c
Pradyumna Paddu <97731439+PADDU-MTKF@users.noreply.github.com>
2024-04-21 16:25:56 +0530

    login save

--- home/views.py
+++ home/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render,HttpResponse
+from django.contrib import messages
+
 
 # Create your views here.
-def home(request):
+
+USERNAME="hello"
+PASSWORD="hi"
+def logout(request):
+    data={'logOut':True}
+    return render(request,'login.html',data)
+
+
+def login(request):
+    if request.method == 'POST':
+        data={}
+        if(request.POST.get('username')==USERNAME and request.POST.get('password')==PASSWORD):
+            data['username'] = request.POST.get('username')
+            data['password'] = request.POST.get('password')
+            data['saveLogin']=True
+            return render(request, 'adminHome.html',data)
+        
+        else:
+            messages.error(request, 'Login Failed ! Please Check your credentials...')
+            return render(request, 'login.html')
+            
+        
+        
+    
+    # data={"username":"admin", "password":"admin"}
     return render(request, 'login.html')
