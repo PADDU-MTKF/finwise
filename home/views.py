8e75b9c9d55e7bbefe05d8df896824bf8f63e9ad
Pradyumna Paddu <97731439+PADDU-MTKF@users.noreply.github.com>
2024-04-22 17:13:51 +0530

    temp

--- home/views.py
+++ home/views.py
@@ -1,45 +1,62 @@
 import os
-from django.shortcuts import render,HttpResponse,redirect
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from . import data as db
 from appwrite.query import Query
 
 
-# Create your views here.
+ADMIN_ENDPOINTS={"home":"adminHome.html","temp":"temp.html"}
+USER_ENDPOINTS={"home":"userHome.html"}
 
-USERNAME="hello"
-PASSWORD="hi"
+# function to handle user logout
 def logout(request):
-    data={'logOut':True}
-    return render(request,'login.html',data)
+    data = {'logOut': True}
+    return render(request, 'login.html', data)
 
-
+# function to handle user login
 def login(request):
     if request.method == 'POST':
-        data={}
-        username=request.POST.get("username")
-        pswd=request.POST.get("password")
-        page=request.POST.get("page")
+        data = {}
         
-        userdet,_=db.getDocument(os.getenv('DB_ID'),os.getenv('LOGIN_ID'),[Query.equal("username", [username]), Query.equal("password", [pswd])])
-        
-        if userdet:
+        # accessing the form details
+        username = request.POST.get("username")
+        pswd = request.POST.get("password")
+        page = request.POST.get("page")
+
+        # Querying the database for user details
+        user_det, _ = db.getDocument(os.getenv('DB_ID'), os.getenv('LOGIN_ID'), [
+                                      Query.equal("username", [username]), Query.equal("password", [pswd])])
+
+        if user_det:
+            # Setting user data for rendering templates
             data['username'] = request.POST.get('username')
             data['password'] = request.POST.get('password')
-            data['saveLogin']=True
-            
-    
-            if userdet[0]['isadmin']:
-                return render(request, 'adminHome.html',data)
+            data['saveLogin'] = True
 
-            return render(request, 'userHome.html',data)
-        
+            # Redirecting to admin or user home page based on user role
+            if user_det[0]['isadmin']:
+                try:
+                    return render(request, ADMIN_ENDPOINTS[page], data)
+                except:
+                    return render(request, ADMIN_ENDPOINTS["home"], data)
+                    
+            else:
+                try:
+                    return render(request, USER_ENDPOINTS[page], data)
+                except:
+                    return render(request, USER_ENDPOINTS["home"], data)
+                
+
+        # If login fails, display error message and redirect to login page
         messages.error(request, 'Login Failed ! Please Check your credentials...')
         return redirect('login')
-        
-        
-        
-    
-    # data={"username":"admin", "password":"admin"}
+
+    # If request method is not POST, render login page
     return render(request, 'login.html')
+
+
+def temp(request):
+    data={"page":"temp"}
+    return render(request, 'login.html', data)
+    
 
